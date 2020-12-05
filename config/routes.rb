c8fbc8427674b319ccba5c1e37c5c04bf838c8c1
Shintaro Matsumoto <45116991+shinter61@users.noreply.github.com>
2020-12-06 00:49:04 +0900

    capistranoでマイグレーション (#4)
    * Comment out ci limit
    
    * Add db role
    
    * Add devise_for
    
    * Limit ci branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#home'
 
-  # devise_for :users
+  devise_for :users
 
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth'
