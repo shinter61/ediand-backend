23e211e5a05e8a79a54ffe02b1c29fad238fc0a9
Shintaro Matsumoto <45116991+shinter61@users.noreply.github.com>
2020-12-05 16:59:08 +0900

    ログインAPI実装 (#2)
    * Setup development environment
    
    * Add favicon.ico
    
    * Create user model
    
    * Regist user
    
    * Persistence local mysql data
    
    * Reflect file change event on docker and rails 6
    
    * Enable devise methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
   def home
-    render :json => { name: "ediand-backend!!" }
+    render :json => { name: User.all, login: user_signed_in? }
   end
 end
